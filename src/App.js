f8961fdd5414d9142e6b76fee90839e314bac342
John Paul Dala <jaypeedala31@gmail.com>
2020-11-19 12:01:51 +0800

    initial commit, module state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, useState } from 'react';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+// const App = props => {
+
+  state = {
+    persons: [
+      { name: 'Jp', age: 21},
+      { name: 'Edwin', age: 41},
+      { name: 'Ruzzle', age: 22},
+    ]
+  };
+
+  // useState('Test');
+  // const [otherState, setOtherState] = useState('some other value');
+  // console.log(personsState, otherState);
+  // useState({otherState: })
+  switchNameHandler = () => {
+    console.log("Was Clicked!");
+    // this.state.persons[0].name = "Maximilian";
+    this.setState({
+      persons: [
+        { name: 'Jp', age: 22},
+        { name: 'Bryan', age: 42},
+        { name: 'Ruzzle', age: 23},
+      ]
+    })
+  }
+  render(){
+    return (
+        <div className="App">
+            <h1> Hi, I'm React JS</h1>
+
+            <button onClick={this.switchNameHandler}>Switch Name</button>
+
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}
+              />
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}>My Hobbies: Golf 
+            </Person>
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}
+            />
+        </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1> Hi, I'm React JS</h1>
+//     </div>
+//   );
+// }
 
 export default App;
+
+// state = {
+//   persons: [
+//     { name: 'Jp', age: 21},
+//     { name: 'Edwin', age: 41},
+//     { name: 'Ruzzle', age: 22},
+//   ],
+//   otherState: 'some value'
+// }
+
+// switchNameHandler = () => {
+//   console.log("Was Clicked!");
+//   // this.state.persons[0].name = "Maximilian";
+//   this.setState({
+//     persons: [
+//       { name: 'Jp', age: 22},
+//       { name: 'Bryan', age: 42},
+//       { name: 'Ruzzle', age: 23},
+//     ]
+//   })
+// }
---
+++ src/Person/Person.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age}</p>
+            <p> {props.children} </p>
+        </div>
+    )
+}
+
+export default person;
+// class Person extends Component{
+//     render(){
+//         return (
+
+//         )
+//     }
+// }
